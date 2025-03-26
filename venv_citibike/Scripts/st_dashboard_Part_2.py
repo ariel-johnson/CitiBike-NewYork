bb39775f976cec124d7862e3acb759d60646f82a
ariel-johnson <akjohnson400@gmail.com>
2025-03-26 19:43:45 -0400

    Update st_dashboard_Part_2.py

--- venv_citibike/Scripts/st_dashboard_Part_2.py
+++ venv_citibike/Scripts/st_dashboard_Part_2.py
@@ -1,7 +1,3 @@
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly"])
-
 ################################################ NEW YORK CITIBIKES DASHABOARD #####################################################
 
 import streamlit as st
